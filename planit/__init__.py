16dc2acceb66d114d3ef5008f590ebfa3977551f
Livo <olivier.pouech@epitech.eu>
2025-06-26 17:33:09 +0200

    restart

--- planit/__init__.py
+++
@@ -1,2 +0,0 @@
-"""PlanIt - Terminal Task Scheduler"""
-__version__ = "0.1.0"
--- tests/test_basic.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from planit.models.task import Task, Priority, TaskStatus
-
-def test_task_creation():
-    """Test basique de création de tâche"""
-    # Ce test sera étendu quand on aura la base de données
-    assert Priority.HIGH.value == "high"
-    assert TaskStatus.TODO.value == "todo"
